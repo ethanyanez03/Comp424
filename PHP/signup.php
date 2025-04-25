f6ac6a536c90b1751ee1f16a2e889106935a84d7
kevinpchco1 <kevin.pacheco.689@my.csun.edu>
2025-04-24 23:34:17 -0700

    update php pt2

--- PHP/signup.php
+++ PHP/signup.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "authentication.c6xqcw662dx5.us-east-1.rds.amazonaws.com";
+$servername = "user.c6xqcw662dx5.us-east-1.rds.amazonaws.com";
 $username = "admin";
 $password = "5prin32025Gr0up";
 $dbname = "comp424";
